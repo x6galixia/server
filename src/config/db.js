1a7c509dc17d8b1c84d548fb843987ee14b2a7f1
x6galixia <x6galixia@gmail.com>
2025-01-15 21:56:34 +0800

    updated

--- src/config/db.js
+++ src/config/db.js
@@ -10,11 +10,15 @@ const pool = new Pool({
     port: process.env.DB_PORT || 5432,         // Database port (default is 5432)
     max: 20,                                  // Max number of clients in pool
     idleTimeoutMillis: 30000,                 // Idle time before a client is disconnected
-    connectionTimeoutMillis: 2000             // Timeout before throwing an error if no connection is made
+    connectionTimeoutMillis: 2000,            // Timeout before throwing an error if no connection is made
+    ssl: process.env.NODE_ENV === 'production' 
+        ? { rejectUnauthorized: false }       // Use SSL in production
+        : false  
+        //THIS IS THE SSL CONFIGURATION IF DO HAVE A SSL CERT   
+        // ssl: {
+        //     ca: fs.readFileSync('path_to_rds_cert.pem').toString(),
+        // }                          // Disable SSL for development
 });
-
-// Export the pool for query execution
-module.exports = pool;
 
 // Error handling
 pool.on('error', (err, client) => {
@@ -22,3 +26,5 @@ pool.on('error', (err, client) => {
     process.exit(-1);  // Exit process if an error occurs
 });
 
+// Export the pool for query execution
+module.exports = pool;
